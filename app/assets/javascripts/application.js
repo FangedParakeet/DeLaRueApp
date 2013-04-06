05769e7399cf7a198d9a119ea136bfdd0626f4c5
Samir Merchant <fanged.parakeet@gmail.com>
2013-04-05 19:42:41 -0700

    more changes for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,9 @@
 //= require underscore
 //= require backbone
 //= require de_la_rue_app
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
+// = require_tree ../templates
+// = require_tree ./models
+// = require_tree ./collections
+// = require_tree ./views
+// = require_tree ./routers
 //= require_tree .
